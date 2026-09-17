7dc1984b94f8bbf8f05d5b236c8642d4cecc0e6b
Soyeong Park <mb535622@gmail.com>
2026-09-17 15:48:53 +0900

    feat: getKakaoAuthUrl에 state 파라미터 추가

--- src/features/auth/utils/getKakaoAuthUrl.ts
+++ src/features/auth/utils/getKakaoAuthUrl.ts
@@ -1,8 +1,9 @@
-export function getKakaoAuthUrl() {
+export function getKakaoAuthUrl(state: string) {
   const params = new URLSearchParams({
     client_id: process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID ?? "",
     redirect_uri: process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI ?? "",
     response_type: "code",
+    state,
   });
 
   return `https://kauth.kakao.com/oauth/authorize?${params.toString()}`;
