0e9479fdd74b47cd1fadd36cb9380c34e59f0dd6
Soyeong Park <mb535622@gmail.com>
2026-09-18 16:49:19 +0900

    feat: 온보딩 완료 페이지 UI 구현

---
+++ src/app/onboarding/complete/page.tsx
@@ -0,0 +1,5 @@
+import { OnboardingCompleteContainer } from "@/features/account/components/OnboardingCompleteContainer";
+
+export default function OnboardingCompletePage() {
+  return <OnboardingCompleteContainer amount={10_000_000} isAiDelegated />;
+}
