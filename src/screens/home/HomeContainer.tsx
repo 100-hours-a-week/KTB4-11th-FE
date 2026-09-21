717a5696b93b38f5ef8fc4be880c36968eada191
Soyeong Park <mb535622@gmail.com>
2026-09-21 21:39:07 +0900

    feat: 홈 화면에 계좌 잔액/주문내역 조합

--- src/screens/home/HomeContainer.tsx
+++ src/screens/home/HomeContainer.tsx
@@ -1,5 +1,7 @@
 import { Header } from "@/shared/components/Header";
+import { MenuRow } from "@/shared/components/MenuRow";
 import { StockTickerBar } from "@/shared/components/StockTickerBar";
+import { AccountBalanceCard } from "@/features/account/components/AccountBalanceCard";
 import { AccountSelector } from "@/screens/home/AccountSelector";
 import SearchIcon from "@/assets/icons/fill/search.svg";
 import ProfileIcon from "@/assets/icons/fill/profile.svg";
@@ -25,6 +27,12 @@ export function HomeContainer() {
       />
       <StockTickerBar title="스톡스푼" />
       <AccountSelector />
+      <div className="flex flex-col gap-2 px-6">
+        <AccountBalanceCard cashBalance={1_240_000} valuation={12_480_000} />
+        <div className="bg-bg-layer-default rounded-2xl px-4">
+          <MenuRow label="주문내역" />
+        </div>
+      </div>
     </div>
   );
 }
