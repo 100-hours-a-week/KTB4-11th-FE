cf5e7d74592baaf0f7aae95ebe1843357dffee36
Soyeong Park <mb535622@gmail.com>
2026-09-22 17:08:10 +0900

    style: 스크롤바 숨김 처리

--- src/shared/components/MobileContainer.tsx
+++ src/shared/components/MobileContainer.tsx
@@ -2,7 +2,7 @@ import type { ReactNode } from "react";
 
 export function MobileContainer({ children }: { children: ReactNode }) {
   return (
-    <div className="bg-bg-layer-basement mx-auto h-full max-w-120 overflow-y-auto">
+    <div className="bg-bg-layer-basement hide-scrollbar mx-auto h-full max-w-120 overflow-y-auto">
       <div className="h-safe-top bg-base-50" />
       {children}
     </div>
