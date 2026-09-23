88159ee43db4ffd975edfd317a7b8fdd712ea421
Soyeong Park <mb535622@gmail.com>
2026-09-23 14:10:54 +0900

    feat: 홈 화면 계좌 선택 트리거에 탑시트 연결

--- src/features/account/components/AccountSelector.tsx
+++ src/features/account/components/AccountSelector.tsx
@@ -1,18 +1,23 @@
+import { AccountSelectSheet } from "@/features/account/components/AccountSelectSheet";
 import ThreeDotHorizontalIcon from "@/assets/icons/fill/three-dot-horizontal.svg";
 import UnfoldIcon from "@/assets/icons/fill/unfold.svg";
 
 export function AccountSelector() {
   return (
     <div className="flex items-center justify-between py-3">
-      <button type="button" className="flex items-center">
-        <span className="border-border-neutral-tertiary mr-2 size-6 rounded-lg border border-dashed" />
-        <span className="heading-1-semibold mr-1">기본계좌</span>
-        <UnfoldIcon
-          width={16}
-          height={24}
-          className="text-icon-neutral-strong"
-        />
-      </button>
+      <AccountSelectSheet
+        trigger={
+          <button type="button" className="flex items-center">
+            <span className="border-border-neutral-tertiary mr-2 size-6 rounded-lg border border-dashed" />
+            <span className="heading-1-semibold mr-1">기본계좌</span>
+            <UnfoldIcon
+              width={16}
+              height={24}
+              className="text-icon-neutral-strong"
+            />
+          </button>
+        }
+      />
       <button type="button">
         <ThreeDotHorizontalIcon
           width={24}
