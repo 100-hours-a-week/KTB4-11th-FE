f2b6b4cd7bfe02a46e10b9a529dd93710b11d0d3
Soyeong Park <mb535622@gmail.com>
2026-09-25 19:30:12 +0900

    feat: 마이페이지 UI 구현 (#53)
    * refactor: MenuRow 컴포넌트에 카드 스타일 내장
    
    * feat: 마이페이지 계좌 성과 카드 컴포넌트 구현
    
    * feat: 마이페이지 프로필 카드 컴포넌트 구현
    
    * feat: 마이페이지 UI 구현
    
    * feat: 홈 화면 프로필 아이콘에 마이페이지 라우트 연결

---
+++ src/app/mypage/page.tsx
@@ -0,0 +1,5 @@
+import { MyPageContainer } from "@/screens/mypage/MyPageContainer";
+
+export default function MyPagePage() {
+  return <MyPageContainer />;
+}
--- src/screens/home/HomeContainer.tsx
+++ src/screens/home/HomeContainer.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Link from "next/link";
 import { BottomTabBar } from "@/shared/components/BottomTabBar";
 import { Header } from "@/shared/components/Header";
 import { MenuRow } from "@/shared/components/MenuRow";
@@ -41,11 +42,13 @@ export function HomeContainer() {
               height={24}
               className="text-icon-neutral-primary"
             />
-            <ProfileIcon
-              width={24}
-              height={24}
-              className="text-icon-neutral-primary"
-            />
+            <Link href="/mypage">
+              <ProfileIcon
+                width={24}
+                height={24}
+                className="text-icon-neutral-primary"
+              />
+            </Link>
           </>
         }
       />
@@ -57,9 +60,7 @@ export function HomeContainer() {
             cashBalance={account?.cash_balance ?? 0}
             valuation={account?.holdings_market_value ?? 0}
           />
-          <div className="bg-bg-layer-default rounded-2xl px-4">
-            <MenuRow label="주문내역" />
-          </div>
+          <MenuRow label="주문내역" />
         </div>
         <div className="mt-4">
           <HoldingStockList
