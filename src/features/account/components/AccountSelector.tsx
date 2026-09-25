ae7fd39287993bf10049264a358b222328364cb2
Soyeong Park <mb535622@gmail.com>
2026-09-25 16:25:49 +0900

    feat: 계좌 선택 영역에 실제 계좌 데이터 연동

--- src/features/account/components/AccountSelector.tsx
+++ src/features/account/components/AccountSelector.tsx
@@ -4,10 +4,14 @@ import { useState } from "react";
 import { AccountNameEditModal } from "@/features/account/components/AccountNameEditModal";
 import { AccountSelectSheet } from "@/features/account/components/AccountSelectSheet";
 import { AccountSettingsSheet } from "@/features/account/components/AccountSettingsSheet";
+import { useAccountDetailQuery } from "@/features/account/hooks/useAccountDetailQuery";
+import { useSelectedAccountId } from "@/features/account/hooks/useSelectedAccountId";
 import ThreeDotHorizontalIcon from "@/assets/icons/fill/three-dot-horizontal.svg";
 import UnfoldIcon from "@/assets/icons/fill/unfold.svg";
 
 export function AccountSelector() {
+  const accountId = useSelectedAccountId();
+  const { data: account } = useAccountDetailQuery(accountId);
   const [isEditModalOpen, setIsEditModalOpen] = useState(false);
 
   return (
@@ -16,7 +20,9 @@ export function AccountSelector() {
         trigger={
           <button type="button" className="flex items-center">
             <span className="border-border-neutral-tertiary mr-2 size-6 rounded-lg border border-dashed" />
-            <span className="heading-1-semibold mr-1">기본계좌</span>
+            <span className="heading-1-semibold mr-1">
+              {account?.account_name ?? "기본계좌"}
+            </span>
             <UnfoldIcon
               width={16}
               height={24}
@@ -37,12 +43,14 @@ export function AccountSelector() {
         }
         onEditClick={() => setIsEditModalOpen(true)}
       />
-      <AccountNameEditModal
-        open={isEditModalOpen}
-        onOpenChange={setIsEditModalOpen}
-        defaultValue="기본계좌"
-        onSave={() => setIsEditModalOpen(false)}
-      />
+      {accountId !== undefined && account && (
+        <AccountNameEditModal
+          accountId={accountId}
+          open={isEditModalOpen}
+          onOpenChange={setIsEditModalOpen}
+          defaultValue={account.account_name}
+        />
+      )}
     </div>
   );
 }
